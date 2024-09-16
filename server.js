05b741858e22ad7141dc0306c66bdb623d5fd5f0
e-a-n <ean.director@gmail.com>
2024-09-16 13:11:21 -0700

    initial commit, poc seems to function, will add remote server capabilities for more testing

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const http = require('http');
+const WebSocket = require('ws');
+const path = require('path');
+
+const app = express();
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+wss.on('connection', ws => {
+    console.log('A user connected');
+
+    ws.on('message', message => {
+        // Broadcast the message to all connected clients except the sender
+        wss.clients.forEach(client => {
+            if (client !== ws && client.readyState === WebSocket.OPEN) {
+                client.send(message);
+            }
+        });
+    });
+
+    ws.on('close', () => {
+        console.log('A user disconnected');
+    });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
